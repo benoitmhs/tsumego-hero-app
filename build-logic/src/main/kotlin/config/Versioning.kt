4e65a04529ec541e1f69d852c48b7a6c25f2d1b7
benoitmhs <mrsanglier@proton.me>
2026-02-11 17:12:06 +0100

    version 0.0.2

--- build-logic/src/main/kotlin/config/Versioning.kt
+++ build-logic/src/main/kotlin/config/Versioning.kt
@@ -1,8 +1,8 @@
 package config
 
 internal object Versioning {
-    private const val LOCAL_VERSION_CODE = 1
-    private const val LOCAL_VERSION_NAME = "0.1.0"
+    private const val LOCAL_VERSION_CODE = 2
+    private const val LOCAL_VERSION_NAME = "0.0.2"
 
     internal fun versionCode(): Int = LOCAL_VERSION_CODE
     internal fun versionName(): String = LOCAL_VERSION_NAME
